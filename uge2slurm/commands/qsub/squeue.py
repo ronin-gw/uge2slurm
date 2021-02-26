d24f76d99d790544bd604f2ecad1535c9295ef58
Hayato Anzawa <anzawa@sb.ecei.tohoku.ac.jp>
2021-02-26 17:52:46 +0900

    use get_command_path for squeue

--- uge2slurm/commands/qsub/squeue.py
+++ uge2slurm/commands/qsub/squeue.py
@@ -2,15 +2,20 @@ import subprocess
 import logging
 from collections import defaultdict
 
+from uge2slurm.utils.path import get_command_path
 from uge2slurm.commands import UGE2slurmCommandError
 
 logger = logging.getLogger(__name__)
 
 
 def get_running_jobs():
-    command = ["squeue", "--noheader", "--me", "--format", "%i %j"]
+    command = [None, "--noheader", "--me", "--format", "%i %j"]
+    squeue = get_command_path("squeue")
 
     try:
+        if not squeue:
+            raise FileNotFoundError
+        command[0] = squeue[0]
         res = subprocess.run(command, capture_output=True, timeout=15, check=True,
                              universal_newlines=True)
     except FileNotFoundError:
